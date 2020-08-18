2efba2e146a69c166c7a4d7aa4f20c8f5ef298d6
Andrey <and.isaev.v.@yandex.ru>
2020-08-18 22:46:09 +0300

    My first bot

--- homework.py
+++ homework.py
@@ -2,19 +2,30 @@ import os
 import requests
 import telegram
 import time
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+logging.basicConfig(filename="info.log", level=logging.INFO)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name =  homework.get('homework_name')
+    if not (
+        homework_name and
+        homework and
+        homework.get('status')
+    ):
+        logging.exception(f'Не получается получить информацию о {homework}')
+    if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,26 +33,35 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
-    return homework_statuses.json()
+    if not current_timestamp:
+        current_timestamp = int(time.time())
+    params = {
+        'from_date': current_timestamp
+    }
+    try:
+        homework_statuses = requests.get(url, params=params, headers=headers)
+        return homework_statuses.json()
+    except requests.exceptions.RequestException as err_requests:
+        logging.exception(err_requests, 'ошибка соеднения:(')
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    try:
+        return bot.send_message(chat_id=CHAT_ID, text=message)
+    except requests.exceptions.RequestException as err_requests:
+        logging.exception(err_requests, 'ошибка при отправке сообщения:(')
 
 
 def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(300)
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
